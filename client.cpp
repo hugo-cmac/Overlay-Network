bac3d74328820d3532629fd91b17fbabc8734360
hugo-cmac <hdwd_7@pm.me>
2020-11-22 14:08:56 +0000

    client update

--- client.cpp
+++ client.cpp
@@ -68,7 +68,7 @@ int tcpClientSocket(uint32_t ip, int port){
     int s = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in addr = {AF_INET, htons(port)};
-    addr.sin_addr.s_addr = ip
+    addr.sin_addr.s_addr = ip;
     //inet_pton(AF_INET, ip, &addr.sin_addr); para char*
 
     if (connect(s, (struct sockaddr *)&addr,sizeof(addr)) < 0){
